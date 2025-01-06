509b500c29a32c2f7d67603ad54b52a6b237ab47
Giuseppe <s7679508@studenti.unige.it>
2025-01-06 12:32:37 +0000

    Update second Node

--- scripts/get_last_target.py
+++ scripts/get_last_target.py
@@ -1,24 +1,23 @@
 #! /usr/bin/env python
-
 import rospy
 from assignment_2_2024.msg import PlanningActionGoal
 from assignment_2_2024.srv import Last_Target, Last_TargetResponse
 
-
 last_target = None
-
+    
 def target_Callback(msg):
     global last_target
     last_target = msg
-    rospy.loginfo(f"Received a new goal: {last_target.goal.target_pose}")
-
+    position = msg.goal.target_pose.pose.position
+    rospy.loginfo(f"Received a new goal:\nx = {position.x},\ny = {position.y},\nz = {position.z}")
+  
 def process_target_request(req):
     global last_target
     if last_target is None:
         rospy.logwarn("No target available")
-        return Last_TargetResponse()
+        return Last_TargetResponse(success=False)
     target_info = last_target.goal.target_pose
-    return Last_TargetResponse(target_info)
+    return Last_TargetResponse(target_info, success=True)
     
 def service_last_goal():
     rospy.init_node('get_last_target')
@@ -33,5 +32,5 @@ if __name__ == "__main__":
     try:
         service_last_goal()
     except rospy.ROSInterruptException:
-        print("Program interrupted")
+        rospy.logerr("Program interrupted")
 
